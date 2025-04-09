b33860104510c72f0c6c82793539e58c9c141ebf
Srinadh3435 <srinadhsarma.garimella@gmail.com>
2025-04-09 16:02:25 +0530

    second commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const content = <h1>Namsate Swiggy</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("app"))
+
+root.render(content)
